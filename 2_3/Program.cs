710499153ba19f330caf9a8c9be66cd22bd256fc
Maria <mpriorova@mail.ru>
2022-12-31 19:26:54 +0300

    Программа, определяющая выходные по цифре дня недели

---
+++ 2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+if (number >= 1 && number <= 5)
+{
+    Console.WriteLine("День недели не является выходным");
+}
+
+if (number == 6 || number == 7)
+{
+   Console.WriteLine("День недели является выходным"); 
+}
+
+if (number > 7)
+{
+Console.WriteLine("Введите число от 1 до 7"); 
+}
